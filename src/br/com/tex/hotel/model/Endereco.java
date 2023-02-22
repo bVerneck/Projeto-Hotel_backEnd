c097e0d7462e9ed17a3a59a40c6129e7b0b1b517
Willian73 <will73tur@gmail.com>
2023-02-22 18:43:48 -0300

    * Adição Dos ENUMS TipoLogradouro e Estados * Modificação da classe Endereco para 'pegar' os ENUMs adicionados * Alteração do tipo do atributo 'numeroResidencial' * Inclusção do atributo 'complemento' * Implementação dos métodos equal and hashCode e toString()

--- src/br/com/tex/hotel/model/Endereco.java
+++ src/br/com/tex/hotel/model/Endereco.java
@@ -1,30 +1,34 @@
 package br.com.tex.hotel.model;
 
+import java.util.Objects;
+
+import br.com.tex.hotel.enums.Estado;
+import br.com.tex.hotel.enums.TipoLogradouro;
+
 public class Endereco {
+
 	private Integer id;
-	private String tipoLogradouro;
+	private TipoLogradouro tipoLogradouro;
 	private String logradouro;
-	private String numeroResidencial;
+	private Integer numeroResidencial;
+	private String complemento;
 	private String cep;
 	private String bairro;
 	private String cidade;
-	private String estado;
+	private Estado estado;
+	private String pais;
 
 	private static int contador;
 
 	public Endereco() {
-    	this.id = ++contador;
+		this.id = ++contador;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public String getTipoLogradouro() {
+	public TipoLogradouro getTipoLogradouro() {
 		return tipoLogradouro;
 	}
 
-	public void setTipoLogradouro(String tipoLogradouro) {
+	public void setTipoLogradouro(TipoLogradouro tipoLogradouro) {
 		this.tipoLogradouro = tipoLogradouro;
 	}
 
@@ -36,12 +40,20 @@ public class Endereco {
 		this.logradouro = logradouro;
 	}
 
-	public String getNumeroResidencial() {
+	public Integer getNumeroResidencial() {
 		return numeroResidencial;
 	}
 
-	public void setNumeroResidencial(String numeroResidencial) {
+	public void setNumeroResidencial(Integer numeroResidencial) {
 		this.numeroResidencial = numeroResidencial;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
 	}
 
 	public String getCep() {
@@ -68,12 +80,49 @@ public class Endereco {
 		this.cidade = cidade;
 	}
 
-	public String getEstado() {
+	public Estado getEstado() {
 		return estado;
 	}
 
-	public void setEstado(String estado) {
+	public void setEstado(Estado estado) {
 		this.estado = estado;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco { \nid=" + id + ",\n tipoLogradouro=" + tipoLogradouro.getDescricao() + ", logradouro="
+				+ logradouro + ", numeroResidencial=" + numeroResidencial + ", complemento=" + complemento + ",\n cep="
+				+ cep + ",\n bairro=" + bairro + ",\n cidade=" + cidade + ",\n estado=" + estado.getNome() + ",\n pais="
+				+ pais + "\n}";
 	}
 
 }
