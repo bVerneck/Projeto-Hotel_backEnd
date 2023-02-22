c0b734d1f4b40546539bb4f72876fc86d725b1dd
Willian73 <will73tur@gmail.com>
2023-02-22 18:28:10 -0300

    * Correção validação de CPF * Adção de contador para gerar id automaticamente.
    *Remoção do método 'setId()'

--- src/br/com/tex/hotel/model/Endereco.java
+++ src/br/com/tex/hotel/model/Endereco.java
@@ -2,62 +2,78 @@ package br.com.tex.hotel.model;
 
 public class Endereco {
 	private Integer id;
-    private String tipoLogradouro;
-    private String logradouro;
-    private String numeroResidencial;
-    private String cep;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    
+	private String tipoLogradouro;
+	private String logradouro;
+	private String numeroResidencial;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	private static int contador;
+
+	public Endereco() {
+    	this.id = ++contador;
+	}
+
 	public Integer getId() {
 		return id;
 	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
+
 	public String getTipoLogradouro() {
 		return tipoLogradouro;
 	}
+
 	public void setTipoLogradouro(String tipoLogradouro) {
 		this.tipoLogradouro = tipoLogradouro;
 	}
+
 	public String getLogradouro() {
 		return logradouro;
 	}
+
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
+
 	public String getNumeroResidencial() {
 		return numeroResidencial;
 	}
+
 	public void setNumeroResidencial(String numeroResidencial) {
 		this.numeroResidencial = numeroResidencial;
 	}
+
 	public String getCep() {
 		return cep;
 	}
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+
 	public String getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-    
-    
+
 }
